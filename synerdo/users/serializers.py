ba0a204f1f5cd8d6c25e27f5829d0a0b78c0ace2
zenya451 <104207659+zenya451@users.noreply.github.com>
2025-03-26 12:46:28 +0200

    fix: RegisterSerializer update

--- synerdo/users/serializers.py
+++ synerdo/users/serializers.py
@@ -4,19 +4,12 @@ from .models import CustomUser
 
 class RegisterSerializer(serializers.ModelSerializer):
     password = serializers.CharField(write_only=True)
-    password2 = serializers.CharField(write_only=True)
 
     class Meta:
         model = CustomUser
-        fields = ['id', 'username', 'email', 'date_joined', 'password', 'password2']
+        fields = ['id', 'username', 'email', 'date_joined', 'password']
         read_only_fields = ['id', 'date_joined']
     
-    def validate(self, attrs):
-        if attrs['password'] != attrs['password2']:
-            raise serializers.ValidationError({"password": "Passwords do not match."})
-        return attrs
-    
     def create(self, validated_data):
-        validated_data.pop('password2')
         user = CustomUser.objects.create_user(**validated_data)
         return user
